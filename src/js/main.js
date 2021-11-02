745271cf5f67d6d91125f3aab2c7ba3a66dfe925
RobLfbv <92669641+RobLfbv@users.noreply.github.com>
2021-11-02 17:14:00 +0100

    V4

--- src/js/main.js
+++ src/js/main.js
@@ -3,10 +3,14 @@ var canvas;
 var game;
 
 
-
+//context.fillRect(0, 0, canvas.width, canvas.height);
 var PLAYER_HEIGHT = 69;
 var PLAYER_WIDTH = 34;
-const OBSTACLE_RADIUS = 20;
+const OBSTACLE_HEIGHT = 89;
+const OBSTACLE_WIDTH = 62;
+
+const GROUND_HEIGHT = 265;
+const GROUND_WIDTH = 680;
 
 const JUMP_HEIGHT = 180;
 var player_fall = 5;
@@ -19,9 +23,18 @@ var verif2 = 0;
 
 var sprites = new Image();
 sprites.onload = animate;
-sprites.src = "./src/sprites/CJ_Runing/cj_running.png";
+sprites.src = "./src/sprites/CJ_Running.png";
 var step = 0;
 var etat = 0;
+
+var spritesObstaclesSol = new Image();
+spritesObstaclesSol.onload = animate;
+spritesObstaclesSol.src = "./src/sprites/Tentacules2.png";
+var stepSol = 0;
+var etatSol = 0;
+
+var spritesSol = new Image();
+spritesSol.src = "./src/sprites/Ground1.png";
 
 var vivant = 1;
 
@@ -29,28 +42,42 @@ var vivant = 1;
 
 function draw() {
     var context = canvas.getContext('2d');
-    context.imgaeSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
     //Terrain
-    context.fillStyle = 'black';
+    /*context.fillStyle = 'black';
     context.fillRect(0, 0, canvas.width, canvas.height);
+    */
     context.fillStyle = 'white';
     context.fillRect(0, 0, canvas.width, canvas.height/1.5);
+    drawSol(game.ground1.x,game.ground1.y);
+    drawSol(game.ground2.x,game.ground2.y);
     //Joueur
     drawPerso(0,game.player.y, Math.floor(step));
 
     //Obstacle
-    obstacle();
+    drawObstacleSol(game.obstacle.x,game.obstacle.y,Math.floor(stepSol));
  }
 function drawPerso(x,y,step){
     var context = canvas.getContext('2d');
-    context.imgaeSmoothingEnabled = false;
+    context.imageSmoothingEnabled = false;
     if(etat == 0){
         context.drawImage(sprites,34*step,0,34,72,x,y,34,72);
     }else if(etat == 1){
-        context.drawImage(sprites,27*step,0,27,63,x,y,34,72);
+        context.drawImage(sprites,29*step,0,29,63,x,y,29,63);
     }
 }
 
+function drawObstacleSol(x,y,stepSol){
+    var context = canvas.getContext('2d');
+    context.imageSmoothingEnabled = false;
+    context.drawImage(spritesObstaclesSol,62*stepSol,0,62,89,x,y,62,89);
+}
+
+function drawSol(x,y){
+	var context = canvas.getContext('2d');
+	context.imageSmoothingEnabled = false;
+	context.drawImage(spritesSol,x,y);
+}
 function update(){
     if(etat == 0){
         step+= 0.05;
@@ -61,6 +88,8 @@ function update(){
         step += 0.01;
         if(step >= 2){step -=2}
     }
+	stepSol += 0.1;
+	if(stepSol>=2){stepSol -= 2;}
 }
 
 function animate(){
@@ -69,27 +98,26 @@ function animate(){
 }
 
 function death(){
-	if(game.player.y + PLAYER_HEIGHT>=game.obstacle.y && 
-        game.player.y <= game.obstacle.y && 
-        0<=game.obstacle.x+OBSTACLE_RADIUS && 34>=game.obstacle.x){
+	if(game.player.y + PLAYER_HEIGHT> game.obstacle.y && 
+        game.player.y < game.obstacle.y + OBSTACLE_HEIGHT && 
+        0< game.obstacle.x+OBSTACLE_WIDTH && 29> game.obstacle.x){
 		console.log("TES MORT BOUH TES NUL");
         vivant = 0;
 	}
 
 }
 
-function obstacle(){
-    var context = canvas.getContext('2d');
-    context.imgaeSmoothingEnabled = false;
-    context.fillStyle = 'red';
-    context.beginPath();
-    context.arc(game.obstacle.x, game.obstacle.y, game.obstacle.r, 0, Math.PI*2, false);
-    context.fill();  
-}
-
 function play(){
     if(vivant == 1){
         game.obstacle.x -= game.obstacle.speed.x;
+        game.ground1.x -= game.obstacle.speed.x;
+        game.ground2.x -= game.obstacle.speed.x;
+        if(game.ground1.x<=-865){
+        	game.ground1.x = 865;
+        }
+        if(game.ground2.x<=-865){
+        	game.ground2.x = 865;
+        }
         animate();
         death();
 
@@ -121,7 +149,7 @@ function play(){
   if(verif1 == 1 && verif2 == 1){
    etat = 1;
    drapeau = 1;
-   sprites.src = "./src/sprites/CJ_Jumping/cj_jet.png";
+   sprites.src = "./src/sprites/CJ_Jet.png";
    verif1 = 0;
 }
     //On impose une limite de saut
@@ -143,7 +171,7 @@ function play(){
     	game.player.y += player_fall;//descendre
     }
     if(drapeaubis == 1 && drapeau == 0 && game.player.y>=canvas.height / 1.5 - PLAYER_HEIGHT){
-        sprites.src = "./src/sprites/CJ_Runing/cj_running.png";
+        sprites.src = "./src/sprites/CJ_Running.png";
         etat = 0;
         drapeaubis = 0;
         degre = 0;
@@ -162,14 +190,21 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         //variables concernant les obstacles
         obstacle: {
-            x: canvas.width-OBSTACLE_RADIUS,
-            y: canvas.height/1.5-OBSTACLE_RADIUS,
-            r: OBSTACLE_RADIUS,
+            x: canvas.width-OBSTACLE_WIDTH,
+            y: canvas.height/1.5-OBSTACLE_HEIGHT,
                 speed: {
                     x: 5
                 }
+        },
+        ground1:{
+        	x:0,
+        	y:canvas.height / 1.5
+        },
+        ground2:{
+        	x:865,
+        	y:canvas.height / 1.5
         }
-        
+
     },
         play();    
 
