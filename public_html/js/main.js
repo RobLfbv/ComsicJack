0f9518cfcefd66203f849995ea4e1eecea2c17da
RobLfbv <92669641+RobLfbv@users.noreply.github.com>
2021-11-02 17:16:48 +0100

    V8

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -8,15 +8,15 @@ var PLAYER_HEIGHT = 69;
 var PLAYER_WIDTH = 34;
 
 const OBSTACLE_HEIGHT = 90;
-const OBSTACLE_WIDTH = 68;
+const OBSTACLE_WIDTH = 62;
 
 const OBSTACLE_HEIGHTVOL = 16;
-const OBSTACLE_WIDTHVOL = 31;
+const OBSTACLE_WIDTHVOL = 28;
 
 const GROUND_HEIGHT = 265;
 const GROUND_WIDTH = 680;
 
-const JUMP_HEIGHT = 250;
+const JUMP_HEIGHT = 275;
 var player_fall = 5;
 var degre = 0;
 
@@ -51,10 +51,33 @@ spriteCiel.onload = animate;
 spriteCiel.src = "./images/Sky220.png";
 var stepCiel = 0;
 
-
 var vivant = 1;
 
 var score = 0;
+
+var spriteStart = new Image();
+spriteStart.src = "./images/Start.png";
+
+var start = 0;
+
+var myMusic; 
+
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+
 
 function draw(tabSol, tabVol) {
 	var context = canvas.getContext('2d');
@@ -68,7 +91,7 @@ function draw(tabSol, tabVol) {
     drawSol(game.ground2.x,game.ground2.y);
     //Joueur
     drawPerso(0,game.player.y, Math.floor(step));
-
+    drawBar(degre);
     //Obstacle
     for(var i = 0; i<tabSol.length; i++){
     	drawObstacleSol(tabSol[i].x,tabSol[i].y,Math.floor(stepSol));
@@ -79,6 +102,9 @@ function draw(tabSol, tabVol) {
     context.font = "30px Arial";
     context.fillStyle = 'white';
     context.fillText(score, 600, 30);
+    if(start == 0){
+    	drawStart();
+    }
 }
 function drawPerso(x,y,step){
 	var context = canvas.getContext('2d');
@@ -115,6 +141,18 @@ function drawSky(x,y,stepCiel){
 	var context = canvas.getContext('2d');
 	context.imageSmoothingEnabled = false;
 	context.drawImage(spriteCiel,680*stepCiel,0,680,250,x,y,680,250);
+}
+function drawBar(degre){
+	var context = canvas.getContext('2d');
+	context.fillStyle = 'white';
+    context.fillRect(0, 450, 100, 15);
+    context.fillStyle = 'green';
+    var percent = ((degre-135)/(JUMP_HEIGHT-135))*90;
+    context.fillRect(5,453,percent,9)
+}
+function drawStart(){
+	var context = canvas.getContext('2d');
+	context.drawImage(spriteStart,0,0,286,77);
 }
 
 function update(){
@@ -190,7 +228,6 @@ function little(e){
 
 function detectJump(e){
 	if(drapeaubis == 0 && (e.keyCode == 32 || e.keyCode == 38)){
-		degre = 150;
 		degre += 50;
 		verif1 = 1;
 		document.addEventListener('keyup', e => {
@@ -201,6 +238,7 @@ function detectJump(e){
 }
 
 function jump(){
+	
 	if(verif1 == 1 && verif2 == 1){
 		etat = 1;
 		drapeau = 1;
@@ -211,7 +249,9 @@ function jump(){
     if(degre>JUMP_HEIGHT){
     	degre = JUMP_HEIGHT;
     }
-
+    if(degre<=125){
+    	degre = 135;
+    }
     //On fait sauter le joueur si le joueur à relacher le saut et si Jack est en dessous de la hauteur de saut
     if(drapeau == 1 && game.player.y>= canvas.height / 1.5 - PLAYER_HEIGHT + 10 - degre){
     	game.player.y -= 10;//monter
@@ -252,37 +292,81 @@ function movement(){
 
 }
 
-function play(){
-	var tabSol = [game.obstacle1, game.obstacle2, game.obstacle3, game.obstacle4];
-	var tabVol = [game.obstacleVol1, game.obstacleVol2, game.obstacleVol3, game.obstacleVol4];
-	if(vivant == 1){
-		for(var i = 0; i<tabSol.length; i= i+1){
-			tabSol[i].x -= tabSol[i].speed.x;	
+function obstaclesRetour(tabSol,tabVol){
+	var stk1;
+	for(var i = 0; i<tabSol.length; i++){
+		if(tabSol[i].x<=-500){
+			stk1 = (Math.random()*(1500-1200))+1200;
+			tabSol[i].x = stk1;
 		}
-		for(var a = 0; a<tabVol.length; a= a+1){
-			tabVol[a].x -= tabVol[a].speed.x;	
+	}
+	var stk2;
+	for(var a = 0; a<tabVol.length; a++){
+		if(tabVol[a].x<=-500){
+			stk1 = (Math.random()*(1500-1200))+1200;
+			tabVol[a].x = stk1;
+			stk2 = (Math.random()*230);
+			if(stk2>=25 && stk2<=75){
+				stk2 = canvas.height/1.5-OBSTACLE_HEIGHT - 100;
+			}
+			tabVol[a].y = stk2;
 		}
-		game.ground1.x -= game.obstacle1.speed.x;
-		game.ground2.x -= game.obstacle1.speed.x;
-		if(game.ground1.x<=-856){
-			game.ground1.x = 856;
-		}
-		if(game.ground2.x<=-856){
-			game.ground2.x = 856;
-		}
-		movement();
-		animate(tabSol, tabVol);
-		death(tabSol, tabVol);
-
-
-		score++;
-		requestAnimationFrame(play);
 	}
 }
 
+function play(){
+	if(start == 1){
+		var tabSol = [game.obstacle1, game.obstacle2, game.obstacle3, game.obstacle4];
+		var tabVol = [game.obstacleVol1, game.obstacleVol2, game.obstacleVol3, game.obstacleVol4];
+		if(vivant == 1){
+			for(var i = 0; i<tabSol.length; i= i+1){
+				tabSol[i].x -= tabSol[i].speed.x;	
+			}
+			for(var a = 0; a<tabVol.length; a= a+1){
+				tabVol[a].x -= tabVol[a].speed.x;	
+			}
+			game.ground1.x -= game.obstacle1.speed.x;
+			game.ground2.x -= game.obstacle1.speed.x;
+			if(game.ground1.x<=-856){
+				game.ground1.x = 856;
+			}
+			if(game.ground2.x<=-856){
+				game.ground2.x = 856;
+			}
+			movement();
+			animate(tabSol, tabVol);
+			death(tabSol, tabVol);
+			obstaclesRetour(tabSol, tabVol);
+			score++;
+			requestAnimationFrame(play);
+		}
+	}else{
+		menu();
+		requestAnimationFrame(play);
+	}
+
+}
+
+function menu(){
+	var rect = canvas.getBoundingClientRect(); 
+	document.addEventListener('click', event => {
+		var posX = (event.clientX - rect.left);
+		var posY = (event.clientY - rect.top);
+		if( 0 <= posY && 
+			77 >= posY &&
+			 0<= posX &&
+			  286 >= posX){
+			start = 1;
+			myMusic.play()
+		}
+	});
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-	canvas = document.getElementById('canvas');
-	game ={
+
+		myMusic = new sound("./sounds/CJ_Song.mp3");
+		canvas = document.getElementById('canvas');
+		game ={
         //variables concernant le joueur
         player:{
         	y: canvas.height / 1.5 - PLAYER_HEIGHT + 10
@@ -316,32 +400,32 @@ document.addEventListener('DOMContentLoaded', function () {
         		x: 4
         	}
         },
-         obstacleVol1: {
+        obstacleVol1: {
         	x: canvas.width-OBSTACLE_WIDTH+300,
         	y: canvas.height/1.5-OBSTACLE_HEIGHT - 100,
         	speed: {
-        		x: 4
+        		x: 6
         	}
         },
         obstacleVol2: {
         	x: canvas.width-OBSTACLE_WIDTH+800,
         	y: canvas.height/1.5-OBSTACLE_HEIGHT - 100,
         	speed: {
-        		x: 4
+        		x: 6
         	}
         },
         obstacleVol3: {
         	x: canvas.width-OBSTACLE_WIDTH+1300,
         	y: canvas.height/1.5-OBSTACLE_HEIGHT - 100,
         	speed: {
-        		x: 4
+        		x: 6
         	}
         },
         obstacleVol4: {
         	x: canvas.width-OBSTACLE_WIDTH+1700,
         	y: canvas.height/1.5-OBSTACLE_HEIGHT - 100,
         	speed: {
-        		x: 4
+        		x: 6
         	}
         },
         ground1:{
@@ -354,7 +438,5 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
     },
-
     play();    
-
 });
